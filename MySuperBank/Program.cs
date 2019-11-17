40b7ac35b536f62f08602c384dc2f0047ef02f9e
kellycoles <kellycoles1@gmail.com>
2019-11-17 11:33:44 -0600

    Bank account app working

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kelly", 1_000);
+            Console.WriteLine($"Account {account.Number} was created for: {account.Owner} with ${account.Balance}.");
+            account.MakeWithdrawal(120, DateTime.Now, "gym membership");
+            account.MakeDeposit(300, DateTime.Now, "got paid");
+            account.MakeWithdrawal(50, DateTime.Now, "cable bill");
+            account.MakeWithdrawal(20, DateTime.Now, "water bill");
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
